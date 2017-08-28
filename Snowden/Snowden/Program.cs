302dc7f5d6779c427fdd30bdc0609bc66c907da1
yevgeny sopot <u329131@danfoss.com>
2017-08-28 09:32:36 +0300

    test task

---
+++ Snowden/Snowden/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Snowden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\w)\1";
+            string input = ReadFile();
+            string output = input;
+
+            var matches = Regex.Matches(input, pattern);
+            while (matches.Count > 0)
+            {
+                output = Regex.Replace(output, pattern, String.Empty);
+                Console.WriteLine("Decoding...");
+                Console.WriteLine(output);
+                matches = Regex.Matches(output, pattern);
+            }
+            Console.WriteLine("Done...");
+            Console.ReadLine();
+        }
+
+        public static string ReadFile()
+        {
+            Console.WriteLine("Enter file path:\n");
+            var filePath = Console.ReadLine();
+            string result = string.Empty;
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(filePath))
+                {
+                    result = File.ReadAllText(filePath);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Could not find file.");
+                result = ReadFile();
+            }
+            return result;
+        }
+    }
+}
